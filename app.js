7e429049a48730ea637adbd518c832e91a78ef14
Zahin101 <zahin.rydha@g.bracu.ac.bd>
2023-06-14 02:36:33 +0600

    Header complete

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-Array.from(document.getElementsByTagName('input')).forEach((e, i)=>{
-    e.addEventListener('keyup',(el)=>{
-        // if (e.value.length > 0){
-        //     document.getElementsByClassName('bi-caret-down-fill')[i].style.color = 'yellow';
-        // }
-        // else{
-        //     document.getElementsByClassName('bi-caret-down-fill')[i].style.color = 'black';
-        // }
-        if (e.value.length > 0){
-            document.getElementsByClassName('bi-caret-down-fill')[i].style.transform = 'rotate(180deg)';
+// Array.from(document.getElementsByTagName('input')).forEach((e, i)=>{
+//     e.addEventListener('keyup',(el)=>{
+//         if (e.value.length > 0){
+//             document.getElementsByClassName('bi-caret-down-fill')[i].style.color = 'red';
+//         }
+//         else{
+//             document.getElementsByClassName('bi-caret-down-fill')[i].style.color = 'black';
+//         }
+//         // if (e.value.length > 0){
+//         //     document.getElementsByClassName('bi-caret-down-fill')[i].style.transform = "rotate(180deg)";
+//         // }
+//         // else{
+//         //     document.getElementsByClassName('bi-caret-down-fill')[i].style.transform = "rotate(0deg)";
+//         // }
+//     })
+// }) 
+
+Array.from(document.getElementsByTagName("input")).forEach((e,i)=>{
+    e.addEventListener("keyup",(el)=>{
+        if (e.value.length>0){
+            document.getElementsByClassName("bi-caret-down-fill")[i].style.transform="rotate(180deg)";
         }
         else{
-            document.getElementsByClassName('bi-caret-down-fill')[i].style.transform = 'rotate(0deg)';
+            document.getElementsByClassName("bi-caret-down-fill")[i].style.transform="rotate(0deg)";
         }
     })
-}) 
+})
